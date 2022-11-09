a5d0cd13e8a7b8cbc398e30b1bfa251b2fcb407f
Ben Pettis <bpettis@wisc.edu>
2022-11-09 15:46:28 -0600

    store urls in dictionary instead

--- download-past-data.py
+++ download-past-data.py
@@ -6,7 +6,7 @@ start_date = date(2021, 1, 25)
 end_date = date.today()
 
 dates_list = []
-url_list = []
+url_list = {}
 
 def daterange(start_date, end_date):
     for n in range(int((end_date - start_date).days) + 1):
@@ -30,11 +30,11 @@ def cloud_function():
 
     # Use those dates to create a list of URLs to then download
     for target_date in dates_list:
-        url_list.append('https://ton.twimg.com/birdwatch-public-data/' + target_date + '/notes/notes-00000.tsv')
-        url_list.append('https://ton.twimg.com/birdwatch-public-data/' + target_date + '/noteRatings/ratings-00000.tsv')
-        url_list.append('https://ton.twimg.com/birdwatch-public-data/' + target_date + '/noteStatusHistory/noteStatusHistory-00000.tsv')
-
-    print(f'Created a list of {len(url_list)} URLs')
+        url_list[target_date] = {'notes': '', 'ratings': '', 'noteStatusHistory': ''}
+        url_list[target_date]['notes'] = ('https://ton.twimg.com/birdwatch-public-data/' + target_date + '/notes/notes-00000.tsv')
+        url_list[target_date]['ratings'] = ('https://ton.twimg.com/birdwatch-public-data/' + target_date + '/noteRatings/ratings-00000.tsv')
+        url_list[target_date]['noteStatusHistory'] = ('https://ton.twimg.com/birdwatch-public-data/' + target_date + '/noteStatusHistory/noteStatusHistory-00000.tsv')
+    print(f'Created a dictionary containing {len(url_list)} URLs')
 
 if __name__ == "__main__":
     print('FYI: Script started directly as __main__')
