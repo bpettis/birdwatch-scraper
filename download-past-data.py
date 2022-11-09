11f0147006554c572401247226b31277b2cb8710
Ben Pettis <bpettis@wisc.edu>
2022-11-09 15:47:41 -0600

    display accurate counts in stdout

--- download-past-data.py
+++ download-past-data.py
@@ -29,12 +29,14 @@ def cloud_function():
         dates_list.append(single_date.strftime("%Y/%m/%d"))
 
     # Use those dates to create a list of URLs to then download
+    url_counter = 0
     for target_date in dates_list:
         url_list[target_date] = {'notes': '', 'ratings': '', 'noteStatusHistory': ''}
         url_list[target_date]['notes'] = ('https://ton.twimg.com/birdwatch-public-data/' + target_date + '/notes/notes-00000.tsv')
         url_list[target_date]['ratings'] = ('https://ton.twimg.com/birdwatch-public-data/' + target_date + '/noteRatings/ratings-00000.tsv')
         url_list[target_date]['noteStatusHistory'] = ('https://ton.twimg.com/birdwatch-public-data/' + target_date + '/noteStatusHistory/noteStatusHistory-00000.tsv')
-    print(f'Created a dictionary containing {len(url_list)} URLs')
+        url_counter += 3
+    print(f'Created a dictionary containing URLs for {len(url_list)} dates of past data. It contains {str(url_counter)} total URLs')
 
 if __name__ == "__main__":
     print('FYI: Script started directly as __main__')
