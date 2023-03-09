761e396b7f4e03fbf3a40e22be7709cb60ab2c12
Ben Pettis <bpettis@wisc.edu>
2023-03-09 17:16:53 -0600

    set an alternate earliest start tate

--- import-old-tsv.py
+++ import-old-tsv.py
@@ -23,7 +23,7 @@ import socket
 load_dotenv(find_dotenv()) # load environment variables
 bucket_name = os.environ.get("gcs_bucket_name")
 project_id = os.environ.get("GCP_PROJECT")
-start_date = os.environ.get("START_DATE", "2023, 2, 19")
+start_date = os.environ.get("START_DATE", "2023, 3, 6")
 start_date = datetime.strptime(start_date, '%Y, %m, %d')
 end_date = datetime.today()
 dates_list = []
@@ -253,7 +253,14 @@ def main(event_data, context):
         print(f'Searching for {object}')
         try:
             df = retrieve_tsv(object)
-            df['statusId'] = df[['noteId', 'participantId']].astype(str).apply(lambda x: ''.join(x), axis=1)
+            try:
+                df['statusId'] = df[['noteId', 'noteAuthorParticipantId']].astype(str).apply(lambda x: ''.join(x), axis=1)
+            except:
+                try:
+                    df['statusId'] = df[['noteId', 'participantId']].astype(str).apply(lambda x: ''.join(x), axis=1)
+                except:
+                    df['statusId'] = 'IDERROR' + datetime.now().strftime('%s')
+
             print(df.info())
             print(df)
             logger.log('Now converting dataframe into sql and placing into a temporary table', severity="INFO")
